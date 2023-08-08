b24aefaf623902fda063372f674b768088fa55fb
Valeron <valerashankorenko@yandex.by>
2023-08-08 19:32:45 +0300

    исправление ошибки

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -72,7 +72,7 @@ class CategoryPost(ListView):
         return category.post_set.filter(
             pub_date__lte=timezone.now(),
             is_published=True
-            ).order_by('-pub_date')
+        ).order_by('-pub_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
